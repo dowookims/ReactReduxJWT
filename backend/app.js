9e31fd8e7a3e896e841a8a871460151a1eeb1ad4
dowookims <kaei2574@gmail.com>
2019-06-04 22:26:41 +0900

    Init express

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./db');
+
+mongoose.connect(config.DB, { useNewUrlParser:true }).then(
+  () => { console.log('DB is connected')},
+  err => { console.log('Can not connect to the db'+ err)}
+);
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.send('hello!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on PORT ${PORT}`)
+});
+
